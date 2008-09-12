804e94033eeca13b0b959db5604b0b1e112241fa
xoopscube.modules <xoopscube.modules@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2008-09-12 00:22:27 +0000

    cubeutils

---
+++ trunk/modules/cubeUtils/language/japanese/blocks.php
@@ -0,0 +1,6 @@
+<?php
+define('_MB_CUBE_UTILS_REMEMBERME', 'IDとパスワードを記憶');
+define('_MB_CUBE_UTILS_RNOW', '新規登録');
+define('_MB_CUBE_UTILS_LPASS', 'パスワード紛失');
+define('_MB_CUBE_UTILS_BLOCKNAME', '表示ブロック名');
+?>
