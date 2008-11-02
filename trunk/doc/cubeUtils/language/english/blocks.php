cf65ea2687805b8f634224d3a617ba6b954e153b
xoopscube.modules <xoopscube.modules@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2008-11-02 09:48:54 +0000

    Update Dev Draft 31-10-2008

---
+++ trunk/doc/cubeUtils/language/english/blocks.php
@@ -0,0 +1,6 @@
+<?php
+define('_MB_CUBE_UTILS_REMEMBERME', 'Remember me');
+define('_MB_CUBE_UTILS_RNOW', 'Register now!');
+define('_MB_CUBE_UTILS_LPASS', 'Lost Password?');
+define('_MB_CUBE_UTILS_BLOCKNAME', 'Display Block Name');
+?>
