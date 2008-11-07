92cec5b67ba6ca69a3601e30137705d6c4bb2077
xoopserver <xoopserver@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2008-11-07 10:57:00 +0000

    CSS Frameworks, Docs, Themes (Samples Emastic, YAML)

---
+++ trunk/themes/emastic_demo6/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="emastic 6"
+Depends=Legacy_RenderSystem,legacy
+Url="http://xoopscube.sourceforge.net/"
+Version="2.2"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=XOOPS Cube Project Team
+ScreenShot="screenshot.png"
+Description="CSS Framework : Emastic : 3 columns standard"
+W3C=NG
+
+Licence="BSD"
+
+*/
+?>
