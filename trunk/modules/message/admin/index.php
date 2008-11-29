213019c4d0cd2391be2fa0e8f5e1f7825a08a123
xoopserver <xoopserver@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2008-11-29 00:30:34 +0000

    module message

---
+++ trunk/modules/message/admin/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../../../mainfile.php';
+$root = XCube_Root::getSingleton();
+$root->mController->executeHeader();
+
+$xoopsLogger = $root->mController->getLogger();
+$xoopsLogger->stopTime();
+$root->mController->executeView();
+?>
