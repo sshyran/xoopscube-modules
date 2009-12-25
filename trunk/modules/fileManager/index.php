b7c4c0b85eab1d039b77bed8dda45cb9a73e0cd2
xoopserver <xoopserver@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2009-12-25 15:36:36 +0000

    Update to Legacy 2.2

--- trunk/modules/fileManager/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*=====================================================================
-  (C)2007 BeaBo Japan by Hiroki Seike
-  http://beabo.net/
-=====================================================================*/
-require_once "../../mainfile.php";
-$root =& XCube_Root::getSingleton();
-$root->mController->executeForward(XOOPS_URL.'/index.php');
-?>
--- trunk/modules/profile/language/english/admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("_AD_PROFILE_LANG_DEFINITIONS_DELETE","Remove field");
-define("_AD_PROFILE_LANG_DEFINITIONS_EDIT","Edit field");
-define("_AD_PROFILE_LANG_DEFINITIONS_VIEW","Display field");
-?>
