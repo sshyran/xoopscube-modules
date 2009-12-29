4861dc476dab5d21b91d393db6ed4b2724be0790
xoopserver <xoopserver@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2009-12-29 00:36:11 +0000

    update module Backup

---
+++ trunk/html/modules/backup/admin/report.php
@@ -0,0 +1,64 @@
+<?php
+require_once '../../../include/cp_header.php';
+require_once './header.php';
+
+include_once './function.php';
+
+//	Database Tables and Modules List
+list($table2module, $modules2version, $modulebydir) = myxbu_get_modules_info('color:#0000ff;');
+
+//	Display Header
+xoops_cp_header();
+print '<div class="adminnavi">BackUp &raquo;&raquo; Report</div>';
+print '<table class="outer">';
+print '<tr><th colspan="7">'._AM_MYXBACKUP_REPORT_TITLE.'</th></tr>';
+print '<tr>';
+print '<td class="head">'._AM_MYXBACKUP_LIST_Name.'</td>';
+print '<td class="head">'._AM_MYXBACKUP_LIST_ModName.'</td>';
+print '<td class="head" align="right">'._AM_MYXBACKUP_LIST_Rows.'</td>';
+print '<td class="head" align="right">'._AM_MYXBACKUP_LIST_Data_length.'</td>';
+print '<td class="head" align="right">'._AM_MYXBACKUP_LIST_Avg_row_length.'</td>';
+print '<td class="head" align="right">'._AM_MYXBACKUP_LIST_Data_free.'</td>';
+print '<td class="head">'._AM_MYXBACKUP_LIST_Update_time.'</td>';
+print '</tr>';
+
+//	Main
+$res = $xoopsDB->queryF('SHOW TABLE STATUS FROM `'.XOOPS_DB_NAME.'`');
+$C = '';
+$Total = array('Rows' => 0, 'Data_length' => 0, 'Data_free' => 0, 'Update_time' => '1970-01-01 00:00:00');
+while ($t = $xoopsDB->fetchArray($res)) {
+	if (preg_match('/^'.XOOPS_DB_PREFIX.'_(.*)$/', $t['Name'])) {
+		$C = (($C == ' class="even"') ? ' class="odd"' : ' class="even"');
+		if (isset($table2module[$t['Name']])) {
+			$ModName = $table2module[$t['Name']];
+			if (isset($modulebydir[$t['Name']]) && $modulebydir[$t['Name']]) { $ModName .= ' *'; }
+		} else {
+			$ModName = '<span style="color:#ff0000;">[ unknown ]</span>';
+		}
+		print '<tr'.$C.'>';
+		print '<td nowrap="nowrap">'.htmlspecialchars($t['Name']).'</td>';
+		print '<td nowrap="nowrap">'.$ModName.'</td>';
+		print '<td nowrap="nowrap" align="right">'.number_format($t['Rows']).'</td>';
+		print '<td nowrap="nowrap" align="right">'.number_format($t['Data_length']).'</td>';
+		print '<td nowrap="nowrap" align="right">'.number_format($t['Avg_row_length']).'</td>';
+		print '<td nowrap="nowrap" align="right">'.number_format($t['Data_free']).'</td>';
+		print '<td nowrap="nowrap">'.$t['Update_time'].'</td>';
+		print '</tr>';
+		$Total['Rows'] += intval($t['Rows']);
+		$Total['Data_length'] += intval($t['Data_length']);
+		$Total['Data_free'] += intval($t['Data_free']);
+		if ($Total['Update_time'] < $t['Update_time']) { $Total['Update_time'] = $t['Update_time']; }
+	}
+}
+print '<tr><th nowrap="nowrap" colspan="2">Total</th>';
+print '<td class="head" nowrap="nowrap" align="right">'.number_format($Total['Rows']).'</td>';
+print '<td class="head" nowrap="nowrap" align="right">'.number_format($Total['Data_length']).'</td>';
+print '<td class="head" nowrap="nowrap" align="right">'.number_format($Total['Data_length'] / $Total['Rows']).'</td>';
+print '<td class="head" nowrap="nowrap" align="right">'.number_format($Total['Data_free']).'</td>';
+print '<td class="head" nowrap="nowrap">'.$Total['Update_time'].'</td>';
+print '</tr>';
+
+//	Display Footer
+print '</table>';
+xoops_cp_footer();
+?>
---
+++ trunk/html/modules/backup/language/english/admin.php
@@ -0,0 +1,75 @@
+<?php
+if (!defined('XOOPS_ROOT_PATH')) { exit(); }
+
+//	General
+define ("_AM_MYXBACKUP_GOBACK_TO_MENU",	"Go Back to Menu");
+define ("_AM_MYXBACKUP_ERROR",		"Error !");
+define ("_AM_MYXBACKUP_PARAMETERERROR",	"Parameter Error !");
+define ("_AM_MYXBACKUP_DBCHECKERROR",	"Error ! (Database might be changed)");
+define ("_AM_MYXBACKUP_FILECHECKERROR",	"Error ! (SQL files might be changed)");
+define ("_AM_MYXBACKUP_RESTOREERROR",	"<div style=\"color:#ff0000; font-size:larger; margin-left:20px;\">Error ! (SQL files might be changed)</div>");
+
+//	Index (Menu)
+define ("_AM_MYXBACKUP_NOTICEONINDEX",	"Don't click twice !");
+define ("_AM_MYXBACKUP_REPORT_TITLE",	"Report");
+define ("_AM_MYXBACKUP_REPORT_DESC",	"Report about database tables.");
+define ("_AM_MYXBACKUP_OPTIMIZE_TITLE",	"Optimize");
+define ("_AM_MYXBACKUP_OPTIMIZE_DESC",	"Optimize XOOPS Cube database tables.");
+define ("_AM_MYXBACKUP_CHECK_TITLE",	"Check");
+define ("_AM_MYXBACKUP_CHECK_DESC",	"Check XOOPS Cube database tables.");
+define ("_AM_MYXBACKUP_CLEANUP_TITLE",	"Clean Up");
+define ("_AM_MYXBACKUP_CLEANUP_DESC",	"Delete unneccesary tables.");
+define ("_AM_MYXBACKUP_BACKUP_TITLE",	"Back Up Dababase");
+define ("_AM_MYXBACKUP_BACKUP_DESC",	"Back up XOOPS Cube databse tables. Download SQL file zipped.");
+define ("_AM_MYXBACKUP_BACKUPMOD_TITLE","Back Up Module");
+define ("_AM_MYXBACKUP_BACKUPMOD_DESC",	"Back up XOOPS Cube databse tables by module. Download SQL file zipped");
+define ("_AM_MYXBACKUP_DUMP_TITLE",	"Dump");
+define ("_AM_MYXBACKUP_DUMP_DESC",	"Dump XOOPS Cube database tables (SQL text format)");
+define ("_AM_MYXBACKUP_EXPORT_TITLE",	"Export");
+define ("_AM_MYXBACKUP_EXPORT_DESC",	"Export XOOPS Cube database selected tables(CSV format)");
+define ("_AM_MYXBACKUP_RESTORE_TITLE",	"Restore");
+define ("_AM_MYXBACKUP_RESTORE_DESC",	"Restore backuped database tables. Place SQL files into 'sql' directory.");
+define ("_AM_MYXBACKUP_CONFIG_TITLE",	"Settings");
+define ("_AM_MYXBACKUP_CONFIG_DESC",	"General settings.");
+define ("_AM_MYXBACKUP_SQLFILE_EXSITS",	"There are some files in SQL directory.<br />If you don't plan to restore, delete these files.");
+
+//	Report / Check
+define ("_AM_MYXBACKUP_LIST_Prefix",		"PREFIX");
+define ("_AM_MYXBACKUP_LIST_Name",		"NAME");
+define ("_AM_MYXBACKUP_LIST_Table",		"TABLE");
+define ("_AM_MYXBACKUP_LIST_ModName",		"MODULE");
+define ("_AM_MYXBACKUP_LIST_Rows",		"ROWS");
+define ("_AM_MYXBACKUP_LIST_Data_length",	"LENGTH");
+define ("_AM_MYXBACKUP_LIST_Avg_row_length",	"Ave. LENGTH");
+define ("_AM_MYXBACKUP_LIST_Data_free",		"DATA FREE");
+define ("_AM_MYXBACKUP_LIST_Update_time",	"UPDATED");
+define ("_AM_MYXBACKUP_LIST_Status",		"STATUS");
+define ("_AM_MYXBACKUP_LIST_Command",		"OPTION");
+
+//	Optimize
+define ("_AM_MYXBACKUP_OPTIMIZED",		"<span style=\"color:#00ff00;\"> optimized</span>");
+define ("_AM_MYXBACKUP_NOTOPTIMIZED",		"<span style=\"color:#ff0000;\"> not optimized</span>");
+define ("_AM_MYXBACKUP_NONEEDOPTIMIZED",	"<span style=\"color:#000000;\"> Table is already up to date</span>");
+define ("_AM_MYXBACKUP_NOTABLE_OPTIMIZED",	"All table are already up to date");
+
+//	Delete
+define ("_AM_MYXBACKUP_NOTABLES_TO_DELETE",	"No tables found to be deleted");
+define ("_AM_MYXBACKUP_TABLE_DELETED",		" deleted");
+define ("_AM_MYXBACKUP_TABLE_NOTDELETED",	" not deleted");
+define ("_AM_MYXBACKUP_CHECK_TO_DELETE",	"OPTION");
+
+//	Restore
+define ("_AM_MYXBACKUP_NOSQLFILES",		"SQL files not found");
+define ("_AM_MYXBACKUP_SQL_NAME",		"SQL FILE");
+define ("_AM_MYXBACKUP_SQL_PREFIX",		"PREFIX in SQL files");
+define ("_AM_MYXBACKUP_SQL_DATE",		"FILE DATE");
+define ("_AM_MYXBACKUP_SQLPREFIX",		"Input PREFIX in SQL files");
+define ("_AM_MYXBACKUP_MYPREFIX",		"PREFIX");
+define ("_AM_MYXBACKUP_GO_RESTORE",		"START");
+define ("_AM_MYXBACKUP_GO_RESTORE_NOTE",	"Start to restore (unable to cancel operation)");
+define ("_AM_MYXBACKUP_RESTORE_DONE",		"Restore done. Delete SQL files.");
+
+//	Clean Up
+define ("_AM_MYXBACKUP_CAUTIONTODEL",		"<span style=\"color:#ff0000;\">Necessary tables might be listed up in the case of DUPLICATABLE modules.<br />DO NOT DELETE any tables if you are not confident.<br />You should BACK-UP before this operation.</span>");
+define ("_AM_MYXBACKUP_TAKERISK",		"TAKE MY OWN RISK");
+?>
