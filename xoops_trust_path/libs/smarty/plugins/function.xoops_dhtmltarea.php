e42836bd4aebdfc249286fc63b63d11bf6a6d424
xoopserver <xoopserver@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2010-12-21 19:30:04 +0000

    update xoops_trust_path legacy

--- xoops_trust_path/libs/smarty/plugins/function.xoops_dhtmltarea.php
+++ xoops_trust_path/libs/smarty/plugins/function.xoops_dhtmltarea.php
@@ -2,36 +2,37 @@
 /**
  *
  * @package Legacy
- * @version $Id: function.xoops_dhtmltarea.php,v 1.3 2008/09/25 15:12:36 kilica Exp $
+ * @version $Id: function.xoops_dhtmltarea.php,v 1.3 2010/02/22 15:12:36 kilica Exp $
  * @copyright Copyright 2005-2007 XOOPS Cube Project  <http://xoopscube.sourceforge.net/> 
  * @license http://xoopscube.sourceforge.net/license/GPL_V2.txt GNU GENERAL PUBLIC LICENSE Version 2
  *
  * [ToDo]
  * 1) We may have to move this file to other module with following namespace or
- *    package.
+ *	  package.
  * 2) Some users and developers want free elements at $params. For example,
- *    $params['script']... This function have not impletented that yet. At
- *    implementing, we will have to define the rule about sanitizing.
+ *	  $params['script']... This function have not impletented that yet. At
+ *	  implementing, we will have to define the rule about sanitizing.
  * 3) Users can't set class element to this function, because XoopsForm is
- *    used. For format xoops_xxxx functions, we may change XoopsForm class
- *    group.
+ *	  used. For format xoops_xxxx functions, we may change XoopsForm class
+ *	  group.
  * 
  */
 
 /*
  * Smarty plugin
  * -------------------------------------------------------------
- * Type:     function
- * Name:     xoops_dhtmltarea
+ * Type:	 function
+ * Name:	 xoops_dhtmltarea
  * Version:  1.0
- * Date:     Jun 6, 2004
- * Author:   minahito
+ * Date:	 Jun 6, 2004
+ * Author:	 minahito
  * Purpose:  cycle through given values
- * Input:    name = form 'name'.
- *           value = preset value. Set raw value without htmlspecialchars().
- *           id = form 'id'. If it's empty, ID is defined automatically by prefix & name.
- *           cols = amount of cols. (default 50)
- *           rows = amount of rows. (default 5)
+ * Input:	 name = form 'name'.
+ *			 value = preset value. Set raw value without htmlspecialchars().
+ *			 id = form 'id'. If it's empty, ID is defined automatically by prefix & name.
+ *			 cols = amount of cols. (default 50)
+ *			 rows = amount of rows. (default 5)
+ *			 editor = textarea editor type (default bbcode)
  * 
  * Examples: {xoops_dhtmltarea name=message cols=40 rows=6 value=$message}
  * -------------------------------------------------------------
@@ -48,29 +49,47 @@ function smarty_function_xoops_dhtmltarea($params, &$smarty)
 	}
 	$form = null;
 
-    $root =& XCube_Root::getSingleton();
-    $textFilter =& $root->getTextFilter();
+	$root =& XCube_Root::getSingleton();
+	$textFilter =& $root->getTextFilter();
 	if (isset($params['name'])) {
 		//
 		// Fetch major elements from $params.
 		//
-		$name = trim($params['name']);
-		$class = isset($params['class']) ? trim($params['class']) : null;
-		$cols = isset($params['cols']) ? intval($params['cols']) : XOOPS_DHTMLTAREA_DEFAULT_COLS;
-		$rows = isset($params['rows']) ? intval($params['rows']) : XOOPS_DHTMLTAREA_DEFAULT_ROWS;
-		$value = isset($params['value']) ? $textFilter->toEdit($params['value']) : null;
-		$id = isset($params['id']) ? trim($params['id']) : XOOPS_DHTMLTAREA_DEFID_PREFIX . $name;
-
+		$params['name'] = trim($params['name']);
+		$params['class'] = isset($params['class']) ? trim($params['class']) : null;
+		$params['cols'] = isset($params['cols']) ? intval($params['cols']) : XOOPS_DHTMLTAREA_DEFAULT_COLS;
+		$params['rows'] = isset($params['rows']) ? intval($params['rows']) : XOOPS_DHTMLTAREA_DEFAULT_ROWS;
+		$params['value'] = isset($params['value']) ? $textFilter->toEdit($params['value']) : null;
+		$params['id'] = isset($params['id']) ? trim($params['id']) : XOOPS_DHTMLTAREA_DEFID_PREFIX . $params['name'];
+	
 		//
 		// Build the object for output.
 		//
-		$form =& new XoopsFormDhtmlTextArea($name, $name, $value, $rows, $cols);
+		$html = "";
+		switch($params['editor']){
+		case 'html':
+			XCube_DelegateUtils::call("Site.TextareaEditor.HTML.Show", new XCube_Ref($html), $params);
+			break;
+		
+		case 'none':
+			XCube_DelegateUtils::call("Site.TextareaEditor.None.Show", new XCube_Ref($html), $params);
+			break;
+		case 'bbcode':
+		default:
+			XCube_DelegateUtils::call("Site.TextareaEditor.BBCode.Show", new XCube_Ref($html), $params);
+			break;
+		}
+		print $html;
+	
+		/*
+		$form =new XoopsFormDhtmlTextArea($name, $name, $value, $rows, $cols);
 		$form->setId($id);
 		if ($class != null) {
 			$form->setClass($class);
 		}
 		
 		print $form->render();
+		*/
 	}
 }
 
