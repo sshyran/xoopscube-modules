0a6139b23e43f154c124c3ad9c0711a0f3472df9
xoopserver <xoopserver@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2011-08-20 07:53:16 +0000

    merge Naao's code release

--- xoops_trust_path/modules/d3forum/search.php
+++ xoops_trust_path/modules/d3forum/search.php
@@ -14,11 +14,30 @@ if( ! function_exists( 'd3forum_global_search_base' ) ) {
 
 function d3forum_global_search_base( $mydirname , $keywords , $andor , $limit , $offset , $userid )
 {
+
+
 	$myts =& MyTextsanitizer::getInstance() ;
 	$db =& Database::getInstance() ;
 
 	$andor = strtoupper( $andor ) ;
 	$userid = intval( $userid ) ;
+
+	// naao from
+	require_once dirname(__FILE__).'/include/main_functions.php' ;
+	// get all forums
+	$sql = "SELECT forum_id, forum_external_link_format FROM ".$db->prefix($mydirname."_forums") ;
+	$frs = $db->query( $sql ) ;
+	$d3com = array() ;
+	while( $forum_row = $db->fetchArray( $frs ) ) {
+		// d3comment object
+		$temp_forum_id = intval($forum_row['forum_id']);
+		if( ! empty( $forum_row['forum_external_link_format'] ) ) {
+			$d3com[$temp_forum_id] =& d3forum_main_get_comment_object( $mydirname , $forum_row['forum_external_link_format'] ) ;
+		} else {
+			$d3com[$temp_forum_id] = false ;
+		}
+	}
+	// naao to
 
 	// XOOPS Search module
 	$showcontext = empty( $_GET['showcontext'] ) ? 0 : 1 ;
@@ -37,12 +56,15 @@ function d3forum_global_search_base( $mydirname , $keywords , $andor , $limit , 
 		$whr_query .= " (p.subject LIKE '%$word4sql%' OR p.post_text LIKE '%$word4sql%')" ;
 	}
 
-	$sql = "SELECT p.post_id,p.topic_id,p.post_time,p.uid,p.subject,p.html,p.smiley,p.xcode,p.br,$select4con FROM ".$db->prefix($mydirname."_posts")." p LEFT JOIN ".$db->prefix($mydirname."_topics")." t ON t.topic_id=p.topic_id WHERE ($whr_forum) AND ($whr_uid) AND ($whr_query) AND ! topic_invisible ORDER BY p.post_time DESC" ;
+	//$sql = "SELECT p.post_id,p.topic_id,p.post_time,p.uid,p.subject,p.html,p.smiley,p.xcode,p.br,$select4con FROM ".$db->prefix($mydirname."_posts")." p LEFT JOIN ".$db->prefix($mydirname."_topics")." t ON t.topic_id=p.topic_id WHERE ($whr_forum) AND ($whr_uid) AND ($whr_query) AND ! topic_invisible ORDER BY p.post_time DESC" ;
+	
+	//naao
+	$sql = "SELECT p.post_id,p.topic_id,p.post_time,p.uid,p.subject,p.html,p.smiley,p.xcode,p.br,$select4con,t.topic_external_link_id,f.forum_id FROM ".$db->prefix($mydirname."_posts")." p LEFT JOIN ".$db->prefix($mydirname."_topics")." t ON t.topic_id=p.topic_id  LEFT JOIN ".$db->prefix($mydirname."_forums")." f ON t.forum_id = f.forum_id WHERE ($whr_forum) AND ($whr_uid) AND ($whr_query) AND ! topic_invisible ORDER BY p.post_time DESC" ;
 
 	$result = $db->query( $sql , $limit , $offset ) ;
 	$ret = array() ;
 	$context = '' ;
- 	while( list( $post_id , $topic_id , $post_time , $uid , $subject , $html , $smiley , $xcode , $br , $text ) = $db->fetchRow( $result ) ) {
+ 	while( list( $post_id , $topic_id , $post_time , $uid , $subject , $html , $smiley , $xcode , $br , $text , $external_link_id, $forum_id ) = $db->fetchRow( $result ) ) {
 
 		// get context for module "search"
 		if( function_exists( 'search_make_context' ) && $showcontext ) {
@@ -51,13 +73,23 @@ function d3forum_global_search_base( $mydirname , $keywords , $andor , $limit , 
 			$context = search_make_context( $full_context , $keywords ) ;
 		}
 
-		$ret[] = array(
-			'link' => "index.php?post_id=$post_id" ,
-			'title' => $subject ,
-			'time' => $post_time ,
-			'uid' => $uid ,
-			"context" => $context ,
-		) ;
+		// naao from
+		$can_display = true;	//default
+		if( is_object( $d3com[intval($forum_id)]) ) {
+			$d3com_obj = $d3com[intval($forum_id)];
+			if( ( $external_link_id = $d3com_obj->validate_id( $external_link_id ) ) === false ) {
+				$can_display = false;
+			}
+		}
+		if ($can_display == true) {
+			$ret[] = array(
+				'link' => "index.php?post_id=$post_id" ,
+				'title' => htmlspecialchars( $subject, ENT_QUOTES ) ,
+				'time' => $post_time ,
+				'uid' => $uid ,
+				'context' => $context ,
+			) ;
+		}	// naao to
 	}
 	return $ret;
 
