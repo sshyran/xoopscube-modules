8af2e1c909d72dde8696a9af6fd2ce3e744a08c2
xoopscube.modules <xoopscube.modules@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2012-01-08 11:09:14 +0000

    Remove modules bulletin, d3blog. Update Legacy modules

--- trunk/html/modules/d3blog/js/fckexe.js
+++
@@ -1,8 +0,0 @@
-function fckeditor_exec(width, height, url) {
-	var area_width = width;
-	var area_height = height;
-	var xoops_url = url;
-	var oFCKeditor = new FCKeditor( "contents", area_width, area_height, "Default" );
-	oFCKeditor.BasePath = xoops_url + "/common/fckeditor/";
-	oFCKeditor.ReplaceTextarea();
-}
--- trunk/html/modules/d3blog/mytrustdirname.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @version $Id: mytrustdirname.php 40 2007-07-21 06:21:54Z hodaka $
- */
-
-$mytrustdirname = 'd3blog';
-
-?>
