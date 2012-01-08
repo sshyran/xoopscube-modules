e53046805fe41ce8dbfb64feacc8cabd0c506a71
xoopscube.modules <xoopscube.modules@ddbe82f5-ef31-0410-8df3-f5d3ac75758f>
2012-01-08 11:05:28 +0000

    Remove modules bulletin, d3blog

--- xoops_trust_path/libs/smarty/plugins/function.legacy_category_select.php
+++ xoops_trust_path/libs/smarty/plugins/function.legacy_category_select.php
@@ -21,16 +21,25 @@ function smarty_function_legacy_category_select($params, &$smarty)
 	$selectHtml = '';
 
 	$tree = $params['tree'];
+	$selectedValue = $params['selectedValue'];
+	$canSelectBranch = isset($params['canSelectBranch']) ? $params['canSelectBranch'] : true;
 
 	foreach(array_keys($tree) as $key){
 		$pkey = $tree[$key]->getPrimary();
 		$d = method_exists($tree[$key], 'getDepth') ? $tree[$key]->getDepth() : 0;	//depth of tree
-		if($params['selectedValue']==$tree[$key]->getShow($pkey)){
-			$selectHtml .= '<option value="'.$tree[$key]->getShow($pkey).'" selected="selected">';
+		if($selectedValue==$tree[$key]->getShow($pkey)){
+			$optionTag = '<option value="'.$tree[$key]->getShow($pkey).'" selected="selected"%s>';
 		}
 		else{
-			$selectHtml .= '<option value="' .$tree[$key]->getShow($pkey). '">';
+			$optionTag = '<option value="' .$tree[$key]->getShow($pkey). '"%s>';
 		}
+		$disabled = null;
+		if($canSelectBranch===false){
+			if(isset($tree[$key+1]) && $tree[$key+1]->getDepth()>$d){
+				$disabled = ' disabled="disabled"';
+			}
+		}
+		$selectHtml .= sprintf($optionTag, $disabled);
 		for($i=0;$i<$d;$i++){
 			$selectHtml .= '-';
 		}
